31a1e09c34e236b704a8f8b3229129a86623fab5
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-12-11 13:17:49 +0800

    Hide door number on openning

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,87 +1,89 @@
-import { ChallengeManager } from './challenges/eval.mjs';
+import {
+  ChallengeManager
+} from './challenges/eval.mjs';
 
 const challenges = new ChallengeManager();
 
 $(document).ready(function () {
 
-    // Calendar animation adapted from: https://codepen.io/dazulu/pen/ByoWee
+  // Calendar animation adapted from: https://codepen.io/dazulu/pen/ByoWee
 
-    let words = ["Lorem ", "ipsum ", "delor", "sit", "amet", "consect", "adipisci", "elit,", "sed.", "Eiusmod", "tempor", "a", "enim", "minim", "season", "nulla", "dolore", "sint", "id", "est", "laboris", "ut.", "aute", "laborum", "toe"];
+  let words = ["Lorem ", "ipsum ", "delor", "sit", "amet", "consect", "adipisci", "elit,", "sed.", "Eiusmod", "tempor", "a", "enim", "minim", "season", "nulla", "dolore", "sint", "id", "est", "laboris", "ut.", "aute", "laborum", "toe"];
 
-    let message = "";
-    let date = new Date();
-    let day = date.getDate();
-    let month = date.getMonth() + 1;
-    let scrolled = false;
-    let timeDelay = 200;
+  let message = "";
+  let date = new Date();
+  let day = date.getDate();
+  let month = date.getMonth() + 1;
+  let scrolled = false;
+  let timeDelay = 200;
 
-    // function to reveal message
-    let cardReveal = function () {
-        $("#message").text(message).show();
-    }
+  // function to reveal message
+  let cardReveal = function () {
+    $("#message").text(message).show();
+  }
 
-    //day=25; // uncomment to skip to 25
+  //day=25; // uncomment to skip to 25
 
-    // Only work in December
-    if (month === 12) {
-        // Loop through each calendar window
-        $("li").each(function (index) {
-            let adventwindow = index + 1;
-            let item = $(this);
+  // Only work in December
+  if (month === 12) {
+    // Loop through each calendar window
+    $("li").each(function (index) {
+      let adventwindow = index + 1;
+      let item = $(this);
 
-            // Open past windows
-            if (day !== adventwindow && adventwindow < day) {
-                window.setTimeout(function () {
-                    item.children(".door").addClass("open");
-                }, timeDelay);
-            }
+      // Open past windows
+      if (day !== adventwindow && adventwindow < day) {
+        window.setTimeout(function () {
+          item.children(".door").addClass("open");
+        }, timeDelay);
+      }
 
-            // timeout offset for past window opening animation
-            timeDelay += 100;
+      // timeout offset for past window opening animation
+      timeDelay += 100;
 
-            // Add words so far to message variable
-            if (adventwindow <= day) {
-                let word = words[index];
-                $(this).append('<div class="revealed">' + word + '</div>');
-                message = message + " " + word;
-            }
+      // Add words so far to message variable
+      if (adventwindow <= day) {
+        let word = words[index];
+        $(this).append('<div class="revealed">' + word + '</div>');
+        message = message + " " + word;
+      }
 
-            // Add jiggle animation to current day window
-            if (adventwindow === day) {
-                $(this).addClass("current");
-                $(this).addClass("jiggle");
-            }
+      // Add jiggle animation to current day window
+      if (adventwindow === day) {
+        $(this).addClass("current");
+        $(this).addClass("jiggle");
+      }
 
-            // On clicking a window, toggle it open/closed and
-            // handle other things such as removing jiggle and 25th
-            $(this).on("click", function () {
-                if (adventwindow <= day) {
-                    $(this).children(".door").toggleClass("open");
-                }
-
-                $(this).removeClass("jiggle");
-
-                // If 25th, can show the message
-                if (day >= 25 && adventwindow === 25) {
-                    messageReveal();
-
-                    // Animate scroll to message if not already done
-                    if (!scrolled) {
-                        $('html, body').animate({
-                            scrollTop: $("#message").offset().top
-                        }, 2000);
-                        scrolled = true;
-                    }
-                }
-            });
-
-        });
-
-        // If beyond 24, show message
-        if (day >= 26) {
-            messageReveal();
+      // On clicking a window, toggle it open/closed and
+      // handle other things such as removing jiggle and 25th
+      $(this).on("click", function () {
+        if (adventwindow <= day) {
+          $(this).children(".door").toggleClass("open");
         }
+
+        $(this).removeClass("jiggle");
+
+        // If 25th, can show the message
+        if (day >= 25 && adventwindow === 25) {
+          messageReveal();
+
+          // Animate scroll to message if not already done
+          if (!scrolled) {
+            $('html, body').animate({
+              scrollTop: $("#message").offset().top
+            }, 2000);
+            scrolled = true;
+          }
+        }
+      });
+
+    });
+
+    // If beyond 24, show message
+    if (day >= 26) {
+      messageReveal();
     }
+  }
 });
 
 /**
@@ -89,8 +91,8 @@ $(document).ready(function () {
  * @param {*} day The day to trigger
  */
 function showCodeWindow(day) {
-    challenges.loadChallenge(day, loadChallengeData);
-    $( "#code-window-wrapper" ).addClass("show");
+  challenges.loadChallenge(day, loadChallengeData);
+  $("#code-window-wrapper").addClass("show");
 }
 
 /**
@@ -98,48 +100,48 @@ function showCodeWindow(day) {
  * Fills out the window values.
  */
 function loadChallengeData() {
-    $( "#challenge-title" ).text(challenges.challenge.title);
-    $( "#challenge-description").text(challenges.challenge.description);
-    $( "#code-pane" ).val(challenges.challenge.initial);
+  $("#challenge-title").text(challenges.challenge.title);
+  $("#challenge-description").text(challenges.challenge.description);
+  $("#code-pane").val(challenges.challenge.initial);
 }
 
 /**
  * Hides code window when the wrapper is clicked
  */
-$( "#code-window-wrapper" ).click(function(e) {
-    if (e.target === this) $( this ).removeClass("show");
+$("#code-window-wrapper").click(function (e) {
+  if (e.target === this) $(this).removeClass("show");
 });
 
 /**
  * Adds tab functionality to the textarea
  */
-$( "#code-pane" ).keydown(function(e) {
-    if (e.key == 'Tab') {
-        e.preventDefault();
-        const start = this.selectionStart;
-        const end = this.selectionEnd;
-    
-        // set textarea value to: text before caret + tab + text after caret
-        this.value = this.value.substring(0, start) + "    " + this.value.substring(end);
-    
-        // put caret at right position again
-        this.selectionStart = this.selectionEnd = start + 4;
-      }
+$("#code-pane").keydown(function (e) {
+  if (e.key == 'Tab') {
+    e.preventDefault();
+    const start = this.selectionStart;
+    const end = this.selectionEnd;
+
+    // set textarea value to: text before caret + tab + text after caret
+    this.value = this.value.substring(0, start) + "    " + this.value.substring(end);
+
+    // put caret at right position again
+    this.selectionStart = this.selectionEnd = start + 4;
+  }
 });
 
 /**
  * Submits the user code for evaluation
  */
-$( "#code-submit" ).click(function() {
-    const success = challenges.evaluate($( "#code-pane" ).val());
-    if (success) alert("Congratulations! Your solution worked!");
-    else alert("Sorry, your solution didn't work!");
+$("#code-submit").click(function () {
+  const success = challenges.evaluate($("#code-pane").val());
+  if (success) alert("Congratulations! Your solution worked!");
+  else alert("Sorry, your solution didn't work!");
 });
 
 /**
  * For testing the code window show hide
  */
-$( "#test-btn" ).click(function() {
-    const day = $( this ).data("day");
-    showCodeWindow(day);
+$("#test-btn").click(function () {
+  const day = $(this).data("day");
+  showCodeWindow(day);
 });
