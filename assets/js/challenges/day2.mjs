bce66bf4c80e339f301340e55ad738c890a01664
John-McPherson <john-mac1989@hotmail.co.uk>
2021-12-11 18:34:56 +0000

    add day2.mjs from template

---
+++ assets/js/challenges/day2.mjs
@@ -0,0 +1,51 @@
+
+/*
+ * This is a template for the advent challenge definitions.
+ */
+
+// Any required library imports:
+//import { /* imports here */ } from './libs.mjs';
+
+
+export const challenge = {
+    title: "Challenge Title Here",
+    description: "Short Challenge description here",
+
+    // Code to be append to the user submission.
+    // Here it converts the first argument to an input variable,
+    // (so the user can access the first parameter as input instead of arg[0]).
+    boilerPlate: "let input = args[0]; ",
+
+    // Code to be initial placed in the code panel
+    initial: "initial code goes here",
+
+    // The argument list to be sent to user code. Should be an array of arrays.
+    // The outer array is the individual tests, the inner array is the argument
+    // list to be sent to the user code for each test.
+    args: [[]],
+
+    /**
+     * Performs any setup prior to running the tests. Can be used to generate
+     * random inputs.
+     */
+    setup: function() {},
+
+    /**
+     * Iterates through the input list and runs the user code for each test.
+     * @param {function} func 
+     * @returns {Boolean} false if any tests fail, otherwise true.
+     */
+    runTests: function(func) {
+        for (const test of this.args) {
+            if (func(test) != this.test(test)) return false;
+        }
+        return true;
+    },
+
+    /**
+     * Reference implementation. User code should provide the same result
+     */
+    test: function(input) {
+        return false;
+    }
+}
