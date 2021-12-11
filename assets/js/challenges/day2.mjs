ba2f14723ae729fe2977090f6efd09e2438cd494
John-McPherson <john-mac1989@hotmail.co.uk>
2021-12-11 18:39:41 +0000

    add title and description to day2.mjs

--- assets/js/challenges/day2.mjs
+++ assets/js/challenges/day2.mjs
@@ -1,4 +1,3 @@
-
 /*
  * This is a template for the advent challenge definitions.
  */
@@ -8,8 +7,11 @@
 
 
 export const challenge = {
-    title: "Challenge Title Here",
-    description: "Short Challenge description here",
+    title: "Day Two - What's the most popular toy? ",
+    description: "It’s almost Christmas and Santa and his elves still have a lot of toys to make. \
+    But they don’t know where to start. \
+    Write a function that when passed an array of toys works out which is the most popular toy.\
+    Fill in the body of the function to return the correct values",
 
     // Code to be append to the user submission.
     // Here it converts the first argument to an input variable,
@@ -22,20 +24,22 @@ export const challenge = {
     // The argument list to be sent to user code. Should be an array of arrays.
     // The outer array is the individual tests, the inner array is the argument
     // list to be sent to the user code for each test.
-    args: [[]],
+    args: [
+        []
+    ],
 
     /**
      * Performs any setup prior to running the tests. Can be used to generate
      * random inputs.
      */
-    setup: function() {},
+    setup: function () {},
 
     /**
      * Iterates through the input list and runs the user code for each test.
      * @param {function} func 
      * @returns {Boolean} false if any tests fail, otherwise true.
      */
-    runTests: function(func) {
+    runTests: function (func) {
         for (const test of this.args) {
             if (func(test) != this.test(test)) return false;
         }
@@ -45,7 +49,7 @@ export const challenge = {
     /**
      * Reference implementation. User code should provide the same result
      */
-    test: function(input) {
+    test: function (input) {
         return false;
     }
 }
