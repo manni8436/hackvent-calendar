7618adf25a8304ba25c251332cefe5b2e48bade4
adowlin <dowlingalison@gmail.com>
2021-12-11 13:32:06 +0000

    Add code window opening to door onclick function

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
 
   // Calendar animation adapted from: https://codepen.io/dazulu/pen/ByoWee
 
-  let words = ["Lorem ", "ipsum ", "delor", "sit", "amet", "consect", "adipisci", "elit,", "sed.", "Eiusmod", "tempor", "a", "enim", "minim", "season", "nulla", "dolore", "sint", "id", "est", "laboris", "ut.", "aute", "laborum", "toe"];
+  let words = ["Day 1 ", "Day 2 ", "Day 3", "Day 4", "Day 5", "Day 6", "Day 7", "Day 8,", "Day 9", "Day 10", "Day 11", "Day 12", "Day 13", "Day 14", "Day 15", "Day 16", "Day 17", "Day 18", "Day 19", "Day 20", "Day 21", "Day 22", "Day 23", "Day 24", "Day 25"];
 
   let message = "";
   let date = new Date();
@@ -59,6 +59,8 @@ $(document).ready(function () {
       $(this).on("click", function () {
         if (adventwindow <= day) {
           $(this).children(".door").toggleClass("open");
+          const day = $(this).data("day");
+          showCodeWindow(day);
         }
 
         $(this).removeClass("jiggle");
