08bd0e1244eea5c0183e9a982fbac89c97510f5f
adowlin <dowlingalison@gmail.com>
2021-12-12 14:34:17 +0000

    Add day 17 challenge

--- assets/js/challenges/day19.mjs
+++ assets/js/challenges/day19.mjs
@@ -3,24 +3,26 @@
 
 
 export const challenge = {
-    title: "Day 18 - Square Area Calculation",
-    description:"Santa needs to find out the size of the area he needs to fly over to deliver gifts. \
+    title: "Day 19 - Triangle Area Calculation",
+    description:"Uh Oh! Some names have been moved to the naughty list at the last minute!\
+                Now the area that Santa needs deliver gifts to is a perfect triangle, and he needs to find out how big the area of the triangle is. \
                 Luckily, the area is a perfect square! He knows that the length of 1 side of the square is 10,000 KM. \
-                Complete the Javascript function to calculate the area of the square.",
+                We know that the triangle's base = 7000 KM, and it's height = 9000 KM.\
+                Complete the Javascript function to calculate the area of the triangle.",
 
     // Code to be append to the user submission.
     // Here it converts the first argument to an input variable,
     // (so the user can access the first parameter as input instead of arg[0]).
-    boilerPlate: "return squareArea(args[0]);",
+    boilerPlate: "return triangleArea(args[0]);",
 
     // Code to be initial placed in the code panel
-    initial: "function squareArea(side) {\n    return area;\n}",
+    initial: "function triangleArea() {\n    return area;\n}",
 
     // The argument list to be sent to user code. Should be an array of arrays.
     // The outer array is the individual tests, the inner array is the argument
     // list to be sent to the user code for each test.
     args: [
-        [10000],
+        [7000, 9000],
     ],
 
     /**
@@ -40,7 +42,7 @@ export const challenge = {
     runTests: function(func, output) {
         output("Starting tests:\n");
         for (const test of this.args) {
-            output("Running: squareArea(): ");
+            output("Running: triangleArea(): ");
             if (func([...test]) != this.test([...test])) {
                 output("Failed!\n");
                 return false;
@@ -54,8 +56,10 @@ export const challenge = {
     /**
      * Reference implementation. User code should provide the same result
      */
-    test: function(side) {
-        let area = side * side;
+    test: function() {
+        let base = 7000;
+        let height = 9000;
+        let area = (base * height) / 2;
         return area;
     }
 }
