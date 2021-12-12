0bfb99d3b5d2ab3e06fd2db87bef53f500513d37
seanyoung247 <seanyoung247@gmail.com>
2021-12-12 18:50:05 +0000

    feat: Adds sound toggle state saving

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,11 @@ const codeWindow = new CodeWindow(new ChallengeManager(), storage);
 const windowText = ["Day 1", "Day 2", "Day 3", "Day 4", "Day 5", "Day 6", "Day 7", "Day 8", "Day 9", "Day 10", "Day 11", "Day 12", "Day 13", "Day 14", "Day 15", "Day 16", "Day 17", "Day 18", "Day 19", "Day 20", "Day 21", "Day 22", "Day 23", "Day 24", "Day 25"];
 setupAdvent(windowText);
 
+// Save sound settings
+$( "#sound-toggle" ).change(function() {
+  storage.setSoundSetting($( this ).prop("checked"));
+});
+
 // Solve now button. Hides challenge brief and shows code panel
 $("#show-code-panel").click(function() {
   codeWindow.showPage("code");
