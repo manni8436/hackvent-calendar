0254429e79d0354bcceb5e2170e14093b08ca538
John-McPherson <john-mac1989@hotmail.co.uk>
2021-12-12 14:43:30 +0000

    add test function to day25.mjs

--- assets/js/challenges/day25.mjs
+++ assets/js/challenges/day25.mjs
@@ -1,8 +1,3 @@
-
-/*
- * This is a template for the advent challenge definitions.
- */
-
 // Any required library imports:
 //import { /* imports here */ } from './libs.mjs';
 
@@ -14,17 +9,9 @@ export const challenge = {
     Write a function that works out exactly how many days there are left until christmas so he knows exactly how panicked he needs to be.\
     Fill in the body of the function to return the correct values",
 
-    // Code to be append to the user submission.
-    // Here it converts the first argument to an input variable,
-    // (so the user can access the first parameter as input instead of arg[0]).
     boilerPlate: "return daysTillXmas(args[0]); ",
+    initial: "function daysTillXmas(date) {\n    return daysTill;\n}",
 
-    // Code to be initial placed in the code panel
-    initial: "function daysTillXmas(args[0]) {\n    return daysTill;\n}",
-
-    // The argument list to be sent to user code. Should be an array of arrays.
-    // The outer array is the individual tests, the inner array is the argument
-    // list to be sent to the user code for each test.
     args: [
         ['1997-11-24T03:24:00'],
         ['1995-12-24T03:24:00'],
@@ -48,29 +35,32 @@ export const challenge = {
     runTests: function(func, output=s=>s) {
         output("Starting tests:\n");
         for (const test of this.args) {
-            output(`Running: ${test}`);
-            if (func(test) != this.test(test)) {
+            output(`Running: milesToKm(${test}): `);
+            if (func(test) != this.test(test)) return false;
+
+            const t1 = Math.round(func(test[0],test[1]));
+            const t2 = Math.round(this.test(test[0],test[1]));
+            
+            if (t1 != t2) {
                 output("Failed!\n");
                 return false;
-            } else {
-                output("Succeeded!\n");
             }
+            output("Succeeded!\n");
         }
         return true;
     },
-
     /**
      * Reference implementation. User code should provide the same result
      */
     test: function(date){
-            date = new Date(date)
-            let year = date.getFullYear()
-            let xmas = new Date(`${year}-12-25T03:24:00`)
+            date = new Date(date);
+            let year = date.getFullYear();
+            let xmas = new Date(`${year}-12-25T03:24:00`);
             if (xmas.getTime() < date.getTime()){
-              xmas = new Date(`${year+1}-12-25T03:24:00`)
+              xmas = new Date(`${year+1}-12-25T03:24:00`);
             }
             
-            let daysTill = (xmas.getTime()-date.getTime())/ 86400000
+            let daysTill = (xmas.getTime()-date.getTime())/ 86400000;
             return daysTill
     }
 }
