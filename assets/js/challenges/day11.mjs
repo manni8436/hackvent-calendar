b30b7dcc938f71aa203e0b8b18579dde6c9f0042
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-12-12 21:43:05 +0800

    Add challenge 11

---
+++ assets/js/challenges/day11.mjs
@@ -0,0 +1,52 @@
+export const challenge = {
+  title: "Day Eleven - Send Christmas Cards",
+  description: "Elves are overwhelmed with all chores that they have to do before the Christmas. \
+                  Help them to send Christmas Cards to people. Santa gave them an example how to do it: \
+                  'Dear Sarah,  Merry Christmas!' \
+                  To do it faster, they need to create a class for all people and use congratulations() method\
+                  Remember that Elves should be polite and use names correctly.' \
+    ",
+
+  boilerPlate: "congratulateStranger(args[0]); ",
+
+  initial: "function congratulateStranger(stranger) {\n    return person.congratulations();\n}",
+
+  args: [
+    'Dan', 'tim', 'Richard', 'mary', 'sunny', 'Daisy', 'mike', 'Chris'
+  ],
+
+  setup: function () {},
+
+
+
+  runTests: function (func, output = s => s) {
+    output("Starting tests:\n");
+    for (const test of this.args) {
+      output(`Running test ${this.args.indexOf(test)}: `);
+
+      const t1 = func(test);
+      const t2 = this.test(test).join(',');
+
+      if (!Array.isArray(t1) || t1.join(',') != t2) {
+        output("Failed!\n");
+        return false;
+      }
+      output("Succeeded!\n");
+    }
+    return true;
+  },
+
+  test: function (stranger) {
+    class Human {
+      constructor(name) {
+        this.name = name
+      }
+      congratulations() {
+        return `Dear ${this.name[0].toUpperCase() + this.name.slice(1)}, Merry Christmas!`
+      }
+    }
+
+    let person = new Human(stranger)
+    return person.congratulations()
+  }
+}
